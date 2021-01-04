7c682dac125a37a9cd775c7055467646b2f82f6e
Lupita Rivera <lupitarivera8899@gmail.com>
2021-01-04 20:43:29 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -20,4 +20,13 @@ return "done";
 }
 
 function doWhileLoop(array) {
-  
+  let i = 0;
+
+function incrementVariable() {
+  i = i + 1;
+  return i;
+}
+
+do {
+  console.log("I run once regardless");
+} while (incrementVariable() < 0);
